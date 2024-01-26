9abab3a6b7fe9c71ab00677ccbf5ef097a57df7a
dmr1991 <65431230+dmr1991@users.noreply.github.com>
2024-01-26 11:15:10 -0800

    Almost responsive
    Footer and details missing

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-          const burgerButton = document.querySelector(".hamburger-button");
-          const burgerLinks = document.querySelector(".nav-links");
-        
-          burgerButton.addEventListener("click", function () {
-            burgerLinks.classList.toggle("active");
-          });
-        });
+$(document).ready(function () {
+  $("nav ul li a:not(:only-child)").click(function (e) {
+    $(this).siblings(".nav-dropdown").toggle();
+    e.stopPropagation();
+  });
+
+  $("html").click(function () {
+    $(".nav-dropdown").hide();
+  });
+  $("#nav-toggle").click(function () {
+    $("nav ul").slideToggle();
+  });
+  $("#nav-toggle").on("click", function () {
+    this.classList.toggle("active");
+  });
+});
